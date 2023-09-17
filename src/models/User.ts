6d4d342cf82c0e1ce0c3f017d477b3f342e06ba0
lucasdwn <lfcosta0804@gmail.com>
2023-09-17 13:16:32 -0300

    Corrigindo erros banco 2

--- src/models/User.ts
+++ src/models/User.ts
@@ -66,6 +66,6 @@ User.init(
 );
 
 User.hasMany(Bicicleta, { foreignKey: 'donoId', as: 'bicicletas' });
-Bicicleta.belongsTo(User, { foreignKey: 'donoId', as: 'dono', constraints: false })
+Bicicleta.belongsTo(User, { foreignKey: 'donoId', as: 'dono'})
 
 export default User;
