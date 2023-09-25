3f7bb1ed476c6639bba53633004ea091be86e1db
lucasdwn <lfcosta0804@gmail.com>
2023-09-24 21:48:33 -0300

    Alternando response de update bicicleta

--- src/controllers/bicicletaController.ts
+++ src/controllers/bicicletaController.ts
@@ -156,7 +156,7 @@ class BicicletaController {
 
       await bicicleta.save();
 
-      return res.status(200).json(bicicleta);
+      return res.status(200).json({ message: 'Bicicleta editada com sucesso.' });
     } catch (error) {
       console.error('Erro ao atualizar uma bicicleta:', error);
       return res.status(500).json({ error: 'Erro interno do servidor.' });
