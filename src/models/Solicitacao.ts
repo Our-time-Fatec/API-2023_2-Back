b1fea0e625bd6b56686528f70617e3bc968e8980
LAB-80 <lucas.robertonascimento01@gmail.com>
2023-10-04 20:06:40 -0300

    commit-solicitacao

--- src/models/Solicitacao.ts
+++ src/models/Solicitacao.ts
@@ -1,7 +1,53 @@
 import { DataTypes, HasManyGetAssociationsMixin, Model, Sequelize } from 'sequelize';
 import sequelize from '../config/database';
+import Bicicleta from './Bicicleta';
 
 class Solicitacao extends Model{
-    public  id!: number;
-
+    public id!: number;
+    public idLocador!: number;
+    public isRespondido!: boolean;
+    public isAceito!: boolean;
+    public bicicletaId!: number;
+    public DataouHora!: Date;
 }
+
+Solicitacao.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        idLocador: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        isRespondido: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+        isAceito: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+        bicicletaId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Bicicletas',
+                key: 'id',
+            },
+            allowNull: false,
+        },
+        DataouHora: {
+            type: DataTypes.DATE,  
+        },
+    },
+    {
+        sequelize,
+        modelName: 'Solicitacao',
+    }
+);
+Solicitacao.belongsTo(Bicicleta, { foreignKey: 'bicicletaId', as: 'bicicleta' });
+
+export default Solicitacao;
