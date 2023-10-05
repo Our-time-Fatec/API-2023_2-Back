a1276efc7bb961a221a5059e5c492328026997ae
andreluke <deco99885@gmail.com>
2023-10-04 22:33:37 -0300

    edicao basica do solicitacaoRoutes comentado

---
+++ src/routes/solicitacaoRoutes.ts
@@ -0,0 +1,13 @@
+import { Router, Request, Response } from "express";
+import authenticateToken from "../middlewares/Auth";
+import SolicitacaoController from "../controllers/SolicitacaoController";
+
+const solicitacaoRoutes = Router()
+
+solicitacaoRoutes.post('/', authenticateToken, SolicitacaoController.createSolicitacao);
+solicitacaoRoutes.get('/:id', authenticateToken, SolicitacaoController.minhaSolicitacao);
+solicitacaoRoutes.get('/:id', authenticateToken, SolicitacaoController.solicitacaoRecebida);
+// solicitacaoRoutes.put('/:id', authenticateToken, SolicitacaoController);
+// solicitacaoRoutes.delete('/:id', authenticateToken, SolicitacaoController);
+
+export default solicitacaoRoutes
