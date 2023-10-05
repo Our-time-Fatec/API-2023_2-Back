78950eff9e4d4d20657c0bacde37fa18c87e111e
LAB-80 <lucas.robertonascimento01@gmail.com>
2023-10-04 22:29:08 -0300

    solicitação controller-2

--- src/controllers/SolicitacaoController.ts
+++ src/controllers/SolicitacaoController.ts
@@ -2,7 +2,18 @@ import { Request, Response} from 'express';
 import Solicitacao from '../models/Solicitacao';
 
 class SolicitacaoController {
+    async createSolicitacao(req: Request, res: Response){
 
+    }
+
+    async minhaSolicitacao(req: Request, res: Response){
+
+    }
+
+    async solicitacaoRecebida(req: Request, res: Response){
+
+    }
 }
 
 export default new SolicitacaoController();
+
