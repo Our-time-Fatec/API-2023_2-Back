a90d6b235e066625ead596c43a51ac5da2a1369c
LucasRbnc <lucas.robertonascimento01@gmail.com>
2023-10-05 21:49:10 -0300

    Controller teste

--- src/controllers/SolicitacaoController.ts
+++ src/controllers/SolicitacaoController.ts
@@ -1,13 +1,35 @@
 import { Request, Response} from 'express';
 import Solicitacao from '../models/Solicitacao';
+import Bicicleta from '../models/Bicicleta';
 
 class SolicitacaoController {
     async createSolicitacao(req: Request, res: Response){
-
+        try{
+        const idLocador = req.body.idLocador;
+        const {
+            DataouHora
+        } = req.body
+        const solicitacao = await Solicitacao.create({
+            idLocador,
+            DataouHora,
+            isAlugada: false
+        });
+        return res.status(201).json({message:"Solicitação enviada com sucesso"});
+        }catch (error){
+            console.error('Erro ao enviar a solicitação', error);
+            return res.status(500).json({error: 'Erro interno do servidor ao enviar a solicitação'});
+        }
     }
 
     async minhaSolicitacao(req: Request, res: Response){
+        try{
+            const isRespondido = req.body
 
+            const solicitacoes = await Solicitacao.findByPk(id, {
+                include: [
+                ]
+            })
+        }
     }
 
     async solicitacaoRecebida(req: Request, res: Response){
