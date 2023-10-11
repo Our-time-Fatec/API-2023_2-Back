d0227b358717dddc74eb0b359ab93ea75e65edf2
LucasRbnc <lucas.robertonascimento01@gmail.com>
2023-10-10 21:32:51 -0300

    update

--- src/controllers/SolicitacaoController.ts
+++ src/controllers/SolicitacaoController.ts
@@ -1,13 +1,16 @@
 import { Request, Response} from 'express';
 import Solicitacao from '../models/Solicitacao';
 import Bicicleta from '../models/Bicicleta';
+import User from '../models/User';
 
 class SolicitacaoController {
     async createSolicitacao(req: Request, res: Response){
         try{
-        const idLocador = req.body.idLocador;
         const {
-            DataouHora
+            DataouHora,
+            idLocador,
+            isRespondido,
+
         } = req.body;
         const solicitacao = await Solicitacao.create({
             idLocador,
@@ -17,7 +20,7 @@ class SolicitacaoController {
         return res.status(200).json({message:"Solicitação enviada com sucesso"});
         }catch (error){
             console.error('Erro ao enviar a solicitação', error);
-            return res.status(500).json({error: 'Erro interno do servidor ao enviar a solicitação'});
+            return res.status(500).json({error: 'Erro interno do servidor'});
         }
     }
 
@@ -25,12 +28,18 @@ class SolicitacaoController {
         try{
             const isRespondido = req.body
             const{
-                idLocador
+                idLocador,
+                idBicicleta,
             } = req.body;
-            const solicitacoes = await Solicitacao.findByPk(idLocador, {
+            const solicitacoes = await Solicitacao.findAll({
                 include: [
-                    { model: Bicicleta, as: 'bicicleta'}
-                ]
+                    { model: Bicicleta, as: 'bicicleta'},
+                    { model:User, as: 'dono',
+                    attributes: {
+                        exclude: ['password'],
+                    },
+                }
+                ],
             });
             return res.status(200).json({solicitacoes});
         }catch (error){
--- src/models/Solicitacao.ts
+++ src/models/Solicitacao.ts
@@ -1,10 +1,12 @@
 import { DataTypes, HasManyGetAssociationsMixin, Model, Sequelize } from 'sequelize';
 import sequelize from '../config/database';
 import Bicicleta from './Bicicleta';
+import User from './User';
 import DouH from '../enums/Data_ou_Hora';
+import { AllowNull } from 'sequelize-typescript';
 
 class Solicitacao extends Model{
-    public id!: number;
+    public idSolicitacao!: number;
     public idLocador!: number;
     public isRespondido!: boolean;
     public isAceito!: boolean;
@@ -14,7 +16,7 @@ class Solicitacao extends Model{
 
 Solicitacao.init(
     {
-        id: {
+        idSolicitacao: {
             type: DataTypes.INTEGER,
             autoIncrement: true,
             primaryKey: true,
@@ -24,7 +26,8 @@ Solicitacao.init(
             references:{
                 model: 'Users',
                 key: 'id',
-            }
+            },
+            allowNull: false,
         },
         isRespondido: {
             type: DataTypes.BOOLEAN,
@@ -52,5 +55,6 @@ Solicitacao.init(
     }
 );
 Solicitacao.belongsTo(Bicicleta, { foreignKey: 'bicicletaId', as: 'bicicleta' });
+Solicitacao.belongsTo(User, {foreignKey: 'idLocador', as: 'id'})
 
 export default Solicitacao;
